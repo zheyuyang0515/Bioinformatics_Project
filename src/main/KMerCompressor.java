1ea9e43d7ac1848a9726d04a57779543b29a95ff
zheyuyang0515 <zheyuyang0515@gmail.com>
2019-11-30 22:39:00 -0500

    get file lines

--- src/main/KMerCompressor.java
+++ src/main/KMerCompressor.java
@@ -1,7 +1,25 @@
 package main;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.LineNumberReader;
+
 public class KMerCompressor {
+	private static int getFileLine(String fileName) throws Exception {
+		File file = new File(fileName);
+		LineNumberReader reader = new LineNumberReader(new FileReader(file));
+		reader.skip(file.length());
+		int lines = reader.getLineNumber();
+		reader.close();
+		return lines;
+	}
 	public static void main(String[] args) {
+		try {
+			System.out.println(getFileLine("SRR065000.fastq"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 }
