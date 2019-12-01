e8313d987af94718c610b17e763108b184a4b30c
zheyuyang0515 <zheyuyang0515@gmail.com>
2019-11-30 19:37:22 -0500

    wavelet tree

--- src/main/WaveletTree.java
+++ src/main/WaveletTree.java
@@ -15,6 +15,31 @@ public class WaveletTree {
 			this.code = code;
 		}
 	}
+	public void insertWT(Map<Character, String> codeTable, WaveletTreeNode root, String L, int len, int level) {
+		char[] lp = new char[L.length()];
+		int lSize = 0;
+		char[] rp = new char[L.length()];
+		int rSize = 0;
+		for(int i = 0; i < len; i++) {
+			String s = codeTable.get(L.charAt(i));
+			char ch = s.charAt(level);
+			if(ch == '1') {
+				root.val += '1';
+				rp[rSize] = L.charAt(i);
+				rSize++;
+			} else {
+				root.val += '0';
+				lp[lSize] = L.charAt(i);
+				lSize++;
+			}
+		}
+		if(root.left != null) {
+			insertWT(codeTable, root.left, String.valueOf(lp), lSize, level + 1);
+		} else if(root.right != null) {
+			insertWT(codeTable, root.right, String.valueOf(rp), rSize, level + 1);
+		}
+		
+	}
 	public WaveletTreeNode constructWT(Map<Character, String> codeTable) {
 		WaveletTreeNode root = new WaveletTreeNode();
 		List<Element> list = new ArrayList<>();
@@ -29,9 +54,22 @@ public class WaveletTree {
 		});
 		for(int i = 0; i < list.size(); i++) {
 			Element element = list.get(i);
-			
+			int len = element.code.length();
+			WaveletTreeNode node = root;
+			for(int j = 0; j < len; j++) {
+				if(element.code.charAt(j) == 0) {
+					if(node.left == null) {
+						node.left = new WaveletTreeNode();
+					}
+					node = node.left;
+				} else {
+					if(node.right == null) {
+						node.right = new WaveletTreeNode();
+					}
+					node = node.right;
+				}
+			}
 		}
 		return root;
-		
 	}
 }
