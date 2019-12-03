b9810fce259175214b465be7da31893c1bbd12e5
zheyuyang0515 <zheyuyang0515@gmail.com>
2019-12-02 23:41:35 -0500

    compress

--- src/main/CompressorThread.java
+++ src/main/CompressorThread.java
@@ -1,24 +1,30 @@
 package main;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.LineNumberReader;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 
 public class CompressorThread implements Runnable{
 	private Thread t;
 	int totalLineNum;
 	int totalBlockNum;
 	String fileName;
-	public CompressorThread(int totalLineNum, String fileName, int totalBlockNum) {
+	String resultFile;
+	public CompressorThread(int totalLineNum, String fileName, int totalBlockNum, String resultFile) {
 		this.totalLineNum = totalLineNum;
 		this.fileName = fileName;
 		this.totalBlockNum = totalBlockNum;
+		this.resultFile = resultFile;
 	}
 	public void start () {
 		if (t == null) {
@@ -26,6 +32,70 @@ public class CompressorThread implements Runnable{
 	        t.start ();
 	    }
     }
+	public static byte[] file2byte(String path)
+    {
+        try {
+            FileInputStream in =new FileInputStream(new File(path));
+            byte[] data=new byte[in.available()];
+            in.read(data);
+            in.close();
+            return data;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+	public synchronized static void traverseWT(WaveletTreeNode root, String output) throws IOException {
+		if(root == null) {
+			return;
+		}
+		Queue<WaveletTreeNode> queue = new LinkedList<>();
+		List<List<Byte>> result = new ArrayList<>();
+		queue.offer(root);
+		Coding c = new Coding();
+		int listSize = 0;
+		while(!queue.isEmpty()) {
+			int size = queue.size();
+			for(int i = 0; i < size; i++) {
+				WaveletTreeNode node = queue.poll();
+				if(node.val.length() != 0) {
+					List<String> runlength = c.runLengthCoding(node.val);
+					List<Byte> compressedCode = node == root ? c.gammaCoding(runlength, node.val, true) : c.gammaCoding(runlength, node.val, false);
+					result.add(compressedCode);
+					listSize += compressedCode.size();
+					/*for(byte b : compressedCode) {
+						//System.out.println(b);
+						writeConcurrent(b, outputStream);
+					}*/
+					if(node.left != null) {
+						queue.offer(node.left);
+					}
+					if(node.right != null) {
+						queue.offer(node.right);
+					}
+				}
+			}
+		}
+		FileOutputStream outputStream  =new FileOutputStream(new File(output), true);
+		writeConcurrent(result, outputStream, listSize);
+		outputStream.close();
+	}
+	private synchronized static void writeConcurrent(List<List<Byte>> result, FileOutputStream outputStream, int size) {
+		byte[] arr = new byte[size];
+		int count = 0;
+		for(List<Byte> list : result) {
+			for(byte b : list) {
+				arr[count] = b;
+				count++;
+			}
+		}
+		try {
+			outputStream.write(arr);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
@@ -67,7 +137,12 @@ public class CompressorThread implements Runnable{
 			WaveletTree waveletTree = new WaveletTree();
 			WaveletTreeNode root = waveletTree.constructWT(codeTable);
 			waveletTree.insertWT(codeTable, root, bwt.L, bwt.L.length(), 0);
-			System.out.println(root.val);
+			try {
+				traverseWT(root, resultFile);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 
--- src/main/KMerCompressor.java
+++ src/main/KMerCompressor.java
@@ -7,6 +7,7 @@ import java.io.LineNumberReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class KMerCompressor {
@@ -19,6 +20,7 @@ public class KMerCompressor {
 		return lines;
 	}
 	public static Map<String, Integer> kmersMap;
+	public static Queue<Byte> queue;
 	private static int blockNum;
 	private static int kmersBlockNum;
 	//set line number++
@@ -31,7 +33,7 @@ public class KMerCompressor {
 		return kmersBlockNum - 1;
 	}
 	public static void main(String[] args) throws Exception {
-		if(args.length != 4) {
+		if(args.length != 5) {
 			throw new IllegalArgumentException("Argument Format Error: 4 arguments required!");
 		}
 		kmersMap = new ConcurrentHashMap<>();
@@ -39,6 +41,15 @@ public class KMerCompressor {
 		int threadNum = Integer.parseInt(args[1]);
 		int kmerLen = Integer.parseInt(args[2]);
 		String outputSource = args[3];
+		String resultFile = args[4];
+		File out = new File(outputSource);
+		if(out.exists()) {
+			out.delete();
+		}
+		out = new File(resultFile);
+		if(out.exists()) {
+			out.delete();
+		}
 		blockNum = 0;
 		int totalLineNum = getFileLine(fileName);
 		int totalBlockNum = totalLineNum / 4;
@@ -65,17 +76,29 @@ public class KMerCompressor {
 		}
 		writer.close();
 		System.out.println("output file " + outputSource + " create successfully.");
+		System.out.println("Compressing kmers...");
 		totalLineNum = getFileLine(outputSource);
 		totalBlockNum = totalLineNum / 2;
 		List<Thread> compressorThreadList = new ArrayList<>();
 		kmersBlockNum = 0;
 		for(int i = 0; i < threadNum; i++) {
-			Thread thread = new Thread(new CompressorThread(totalLineNum, outputSource, totalBlockNum));
+			Thread thread = new Thread(new CompressorThread(totalLineNum, outputSource, totalBlockNum, resultFile));
 			thread.start();
 			compressorThreadList.add(thread);
 		}		
 		for(Thread thread : compressorThreadList) {
 			thread.join();
 		}
+		System.out.println("Compress Successfully!");
+		File source= new File(outputSource);
+		File compression = new File(resultFile);
+		long sourceLen = source.length();
+		long compressionLen = compression.length();
+		long compressionRatio = Math.round(((double)compressionLen / (double)sourceLen) * 100);
+		System.out.println("Result: Kmer File: " + sourceLen + " byte. Compression File: " + compressionLen + " byte. Compression ratio: " + compressionRatio + "%.");
+		/*byte[] data = CompressorThread.file2byte(resultFile);
+		for(byte b : data) {
+			System.out.println(b);
+		}*/
 	}
 }
